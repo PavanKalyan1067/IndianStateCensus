5e83a0859e0fa558bc67dbb7f961c4e104d16496
Pavan Kalyan <pavankalyanyadav321@gmail.com>
2022-04-20 18:48:15 +0530

    Checking Records

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -1,73 +1,24 @@
-﻿using NUnit.Framework;
+﻿using System;
 using IndianStateCensus;
+using NUnit.Framework;
+
 namespace NunitIndianStateCensus.Test
 {
-    public class Test
+    class StateCodeTestCases
     {
-        string indianStateHeaders = "State,Population,AreaInSqKm,DensityPerSqKm";
-        string indianStateCensusfilepath = @"/Users/nikhilgoud/Projects/Assaignments/IndianStateCensus/IndianStateCensus/Indian state census data/IndiaStateCensusData.csv";
-        string wrongfiletype = @"/Users/nikhilgoud/Projects/Assaignments/IndianStateCensus/IndianStateCensus/Indian state census data/IndiaStateCensusData.txt";
-        string wrongfile = @"/Users/nikhilgoud/Projects/Assaignments/IndianStateCensus/IndianStateCensus/Indian state census data/IndiaStateCensus.csv";
-        string delimiterwrong = @"/Users/nikhilgoud/Projects/Assaignments/IndianStateCensus/IndianStateCensus/Indian state census data/DelimiterIndiaStateCensusData.csv";
-        string wrongHeaders = @"/Users/nikhilgoud/Projects/Assaignments/IndianStateCensus/IndianStateCensus/Indian state census data/WrongIndiaStateCensusData.csv";
+        string indianStateCodeHeaders = "SrNo,State Name,TIN,StateCode";
+        string indianStateCodefilepath = @"/Users/nikhilgoud/Projects/Assaignments/IndianStateCensus/IndianStateCensus/Indian state census data/IndiaStateCode.csv";
+
         [Test]
         public void GivenCSVfile_ReturnsNumberOfRecord()
         {
             //Arrange
-            int expected = 29;
+            int expected = 37;
             //Act
-            StateCensus stateCensus = new StateCensus();
-            int totalRecords = stateCensus.GetData(indianStateCensusfilepath, indianStateHeaders);
-            //Assert
-            Assert.AreEqual(expected, totalRecords);
-        }
-        [Test]
-        public void GivenCSVfileIncorrect_ReturnsCustomException()
-        {
-            //Arrange
-            int expected = 0;
-            //Act
-            StateCensus stateCensus = new StateCensus();
-            int totalRecords = stateCensus.GetData(wrongfile, indianStateHeaders);
-            //Assert
-            Assert.AreEqual(expected, totalRecords);
-        }
-
-        [Test]
-        public void GivenIncorrectFiletype_ReturnsCustomException()
-        {
-            //Arrange
-            int expected = 0;
-            //Act
-            StateCensus stateCensus = new StateCensus();
-            int totalRecords = stateCensus.GetData(wrongfiletype, indianStateHeaders);
-            //Assert
-            Assert.AreEqual(expected, totalRecords);
-        }
-
-        [Test]
-        public void GivenIncorrectDelimiter_ReturnsCustomException()
-        {
-            //Arrange
-            int expected = 0;
-            //Act
-            StateCensus stateCensus = new StateCensus();
-            int totalRecords = stateCensus.GetData(delimiterwrong, indianStateHeaders);
-            //Assert
-            Assert.AreEqual(expected, totalRecords);
-        }
-
-        [Test]
-        public void GivenIncorrectHeaders_ReturnsCustomException()
-        {
-            //Arrange
-            int expected = 0;
-            //Act
-            StateCensus stateCensus = new StateCensus();
-            int totalRecords = stateCensus.GetData(wrongHeaders, indianStateHeaders);
+            StateCensusAnalyser stateCensus = new StateCensusAnalyser();
+            int totalRecords = stateCensus.GetStateCodeData(indianStateCodefilepath, indianStateCodeHeaders);
             //Assert
             Assert.AreEqual(expected, totalRecords);
         }
     }
 }
-
