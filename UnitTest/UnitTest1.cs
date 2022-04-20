c154c8cda98694d8ead9902f21a3d4fd825aa6e2
Pavan Kalyan <pavankalyanyadav321@gmail.com>
2022-04-20 18:53:36 +0530

    incorrect delimiter passed gives custom exception

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -9,6 +9,8 @@ namespace NunitIndianStateCensus.Test
         string indianStateCodeHeaders = "SrNo,State Name,TIN,StateCode";
         string indianStateCodefilepath = @"/Users/nikhilgoud/Projects/Assaignments/IndianStateCensus/IndianStateCensus/Indian state census data/IndiaStateCode.csv";
         string wrongfile = @"/Users/nikhilgoud/Projects/Assaignments/IndianStateCensus/IndianStateCensus/Indian state census data/StateCode.csv";
+        string wrongDelimiter = @"/Users/nikhilgoud/Projects/Assaignments/IndianStateCensus/IndianStateCensus/Indian state census data/DelimiterIndiaStateCode.csv";
+
         [Test]
         public void GivenCSVfile_ReturnsNumberOfRecord()
         {
@@ -28,6 +30,17 @@ namespace NunitIndianStateCensus.Test
             //Act
             StateCensusAnalyser stateCensus = new StateCensusAnalyser();
             int totalRecords = stateCensus.GetStateCodeData(wrongfiletype, indianStateCodeHeaders);            //Assert
+            Assert.AreEqual(expected, totalRecords);
+        }
+        [Test]
+        public void GivenIncorrectDelimiter_ReturnsCustomException()
+        {
+            //Arrange
+            int expected = 0;
+            //Act
+            StateCensusAnalyser stateCensus = new StateCensusAnalyser();
+            int totalRecords = stateCensus.GetStateCodeData(wrongDelimiter, indianStateCodeHeaders);
+            //Assert
             Assert.AreEqual(expected, totalRecords);
         }
     }
